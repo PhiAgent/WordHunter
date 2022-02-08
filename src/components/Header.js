90ac0ab36c70cb361ff77c024609a83e46e32134
PhiAgent <princegyekyeaddai@gmail.com>
2022-02-08 12:10:28 -0600

    lightDarkModeStarted

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import React, {useState} from 'react';
 import useMode from '../context/GameContext';
 
 const Header = () => {
-  const { darkTheme, toggleTheme } = useMode();
+  const { lightMode, changeMode } = useMode();
 
 
   return (
-    <header className="">
-      Word Game
+    <header className={`title ${lightMode ? "dark" : ""}`}>
+      <h1>Word Game</h1>
+      <button onClick={() => changeMode()}>darkmode</button>
     </header>
   )
 };
